2567fbb20891ccc683e3f31e848e5149ae7a6984
mrabie19 <m.rabie-19@lboro.ac.uk>
2024-01-16 11:18:16 +0000

    Modified data size 150 to 15000. Added Makefile. Added script.

--- script.py
+++ script.py
@@ -4,42 +4,59 @@ import time
 from datetime import datetime
 import numpy as np
 
-program_par = "parallel"
-num_threads = "2"
-matrix_size = "250"
-program_seq = "sequential" # comment/uncomment to toggle between programs
-wdir = "C:\WSD530\CW1\Task1/"
+wdir = "C:\WSD530\OpenMP/"
+app = "codes/part1_cases/"
+# program = "svm"
+
+mla_programs = ["SVM", "NeuralNet", "RandomForest"]
+
+def rebuild():
+    print("Rebuilding programs")
+    compile = subprocess.run("cd " + wdir + app + "&& make clean", shell=True, capture_output=True, text=True)
+    if compile.stderr:
+        print(compile.stderr)
+        exit()
+    compile = subprocess.run("cd " + wdir + app + "&& make", shell=True, capture_output=True, text=True)
+    if compile.stderr:
+        print(compile.stderr)
+        exit()
 
 def main():
-    print("Compiling programs")
-    compile = subprocess.run("cd " + wdir + "Code/" + "&& make clean", shell=True, capture_output=True, text=True)
-    if compile.stderr:
-        print(compile.stderr)
-        exit()
-    compile = subprocess.run("cd " + wdir + "Code/" "&& make", shell=True, capture_output=True, text=True)
-    if compile.stderr:
-        print(compile.stderr)
-        exit()
+    global app
 
-    program = program_par # Select program to run
+    # Rebuild programs
+    rebuild()
+
     # Store runtimes in file
-    f = open(wdir + "Data/" + program + "_" + num_threads + "_" + matrix_size + "_results.txt", "w")
-    print("Collecting " + program + " data")
-    for i in range(100):
-        # Run the executable 
-        log = subprocess.run(wdir + "Code/" + ".\\" + program, shell=True, capture_output=True, text=True)
-        f.write(log.stdout)
-    f.close()
-    print("Completed " + program )
+    for mla in mla_programs:
+        app = "codes/part1_cases/" + f"{mla}/"
+        program = mla
+        output = wdir + "data/" + program + ".txt"
+        exec_times = wdir + "data/" + program + "_results.txt"
 
-    # Repeat for sequential code
-    program = program_seq
-    f = open(wdir + "Data/" + program + "_" + num_threads + "_" + matrix_size + "_results.txt", "w")
-    print("Collecting " + program + " data")
-    for i in range(100):
-        log = subprocess.run(wdir + "Code/" + ".\\" + program, shell=True, capture_output=True, text=True)
-        f.write(log.stdout)
-    f.close()
-    print("Completed " + program)
+        # Overwrite files
+        with open(output, 'w') as file:
+            file.close()
+        with open(exec_times, 'w') as file:
+            file.close() 
+
+        print("Collecting " + program + " data")
+        print("Running subprocess: " + wdir+app + ".\\" + program)
+        for i in range(100):
+            # Run the executable 
+            f = open(output, "w")
+            log = subprocess.run(wdir+app + ".\\" + program, shell=True, capture_output=True, text=True)
+            
+            f.write(log.stdout)
+            f.close()
+            with open(output, 'r') as file:
+                lines = file.readlines()
+                file.close()
+
+            with open(exec_times, 'a') as file:
+                file.write(lines[-1])
+                
+        file.close()
+        print("Completed " + program )
 
 main()
